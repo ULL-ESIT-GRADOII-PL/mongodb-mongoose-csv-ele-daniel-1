2baa9b70a684041dc0f2c80525f1244a6463dd3f
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2016-04-20 16:47:55 +0100

    Comenting

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var app = express();
 var path = require('path');
 var expressLayouts = require('express-ejs-layouts');
 
-
 app.set('port', (process.env.PORT || 5000));
 app.set('ip', (process.env.IP || '0.0.0.0'));
 
@@ -15,21 +14,36 @@ app.use(expressLayouts);
 
 app.use(express.static(__dirname + '/public'));
 
+let server = require('./models/server.js');
 var calculate = require('./models/calculate');
 
 app.get('/', (request, response) => {
 	response.render('index', {title: 'CSV Analizer'});
 });
 
+/*
+ * Mantenemos la misma funcionalidad anterior, se puede usar el servicio
+ */
 app.get('/csv', (request, response) => {
 	response.send({'rows': calculate(request.query.input)});
-    // Solo se envian los datos necesarios, no una vista de ahi a que no se use render
 });
 
+/*
+ * Se devuelve una lista de csv guardados con ese usuario
+ */
 app.get('/users/:id', (request, response) => {
-    response.send({ 'listExamples': getCsvList(request.params.id) });
+    let csv = new server.CsvModel();
+    response.send({ 'listExamples': csv.getCsvList(request.params.id) });
 });
 
+/*
+ * Se obtiene el csv apartir de una peticion del cliente mediante ajax
+ */
+app.get('/csvfile', (request, response) => {
+    let req = request.query;
+    let csv = new server.CsvModel();
+    response.send({'csvfile': csv.loadCsv(request.setAttributeNode(req.user, req.name))});
+});
 
 app.listen(app.get('port'), () => {
     console.log(`Node app is running at localhost: ${app.get('port')}` );
